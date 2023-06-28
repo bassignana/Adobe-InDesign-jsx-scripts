090ee117458fe78a25fc584aeef3e871f2ad668b
bassignana <40897063+bassignana@users.noreply.github.com>
2023-06-28 14:27:32 +0200

    updated rules

--- ConditionalApplyCharStyle.jsx
+++ ConditionalApplyCharStyle.jsx
@@ -1,20 +1,61 @@
 // testo formule: 
-//      alfabeto -> corsivo
-//      numero -> regular
+//      alfabeto piccolo -> corsivo dimensione piccola
+//      alfabeto grande -> corsivo dimensione grande
+//      numero -> regular dimenione numero
 //      greek -> regular
-//      math symbol -> regular
+//      math symbol -> regular stix
 // apice formule:
 //      alfabeto -> corsivo
 //      numero -> regular
 // pedice formule:
 //      come apice
-//
 
 function isAlphabeticalLetter(character) {
   var characterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  for (var i = 0; i < characterList.length; i++) {
+    if (characterList[i] === character) {
+      return true;
+    }
+  }
+
+  return false;
+
+}
+
+function isBigAlphabeticalLetter(character) {
+  var characterList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  for (var i = 0; i < characterList.length; i++) {
+    if (characterList[i] === character) {
+      return true;
+    }
+  }
+
+  return false;
+
+}
+
+function isSmallAlphabeticalLetter(character) {
+  var characterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+
+  for (var i = 0; i < characterList.length; i++) {
+    if (characterList[i] === character) {
+      return true;
+    }
+  }
+
+  return false;
+
+}
+
+function isNum(character) {
+  var characterList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
   for (var i = 0; i < characterList.length; i++) {
     if (characterList[i] === character) {
@@ -51,11 +92,15 @@ for (var i = 0; i < selection.length; i++) {
     }
   } else {
 
-    if (isAl) {
-       c.appliedCharacterStyle = cg.characterStyles.itemByName("10E formula txt italic");       
+    if (isBigAlphabeticalLetter(ch)) {
+       c.appliedCharacterStyle = cg.characterStyles.itemByName("10E formula txt big italic");       
+    } else if (isSmallAlphabeticalLetter(ch)) {
+        c.appliedCharacterStyle = cg.characterStyles.itemByName("10E formula txt italic");
+    } else if (isNum(ch)) {
+        c.appliedCharacterStyle = cg.characterStyles.itemByName("10E formula txt num regular");
     } else {
-        c.appliedCharacterStyle = cg.characterStyles.itemByName("10E formula txt regular");
+        c.appliedCharacterStyle = cg.characterStyles.itemByName("10E formula txt regular"); // symbols like greek and + / - etc etc
+        } 
     }
-  }
 }
 
